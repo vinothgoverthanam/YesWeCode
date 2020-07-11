75c1befbe7d0d00809c6539288ebc1ad22c23947
Goverthanam <vinoth.goverthanam@nokia.com>
2020-07-11 17:44:09 +0530

    array

--- array.py
+++ array.py
@@ -1,1 +1,14 @@
-print "hello world"
+from itertools import  permutations
+
+R=int(input("enter the target number"))
+N=int(input("enter the size of the array :"))
+p="enter the integer"
+lista=[]
+
+for i in range(N) :
+    K=int(input(("%s %d ") % (p,i)))
+    lista.append(K)
+
+solutions = [pair for pair in permutations(lista, 2) if sum(pair) == R]
+print('Solutions:', solutions)
+print (set(map(frozenset, solutions)))
